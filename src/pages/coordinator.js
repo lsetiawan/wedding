f6cde47f2b97646f66855511ab276052de7f198a
lsetiawan <landungs@uw.edu>
2018-06-27 11:42:24 -0700

    Add react-pdf and ceremony layout

--- src/pages/coordinator.js
+++ src/pages/coordinator.js
@@ -6,6 +6,7 @@ const Coordinator = () => {
         { title: 'Reception Schedule', url: '/' },
         { title: 'Day Schedule', url: '/day-schedule/' },
         { title: 'Ceremony Outline', url: '/ceremony-outline/' },
+        { title: 'Ceremony Venue Layout', url: '/ceremony-layout/' },
         { title: 'Inventory List', url: '/inventory/' },
         { title: 'Setup To Do List', url: '/setup/' },
         { title: 'Tear Down To Do List', url: '/tear-down/' }
