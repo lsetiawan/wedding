2fffbbd88373640b8c3c1c050fa1ac1b68e302da
Adrian Prananda <prananda0203@gmail.com>
2018-06-27 20:44:42 -0700

    Added pdf links

--- src/pages/coordinator.js
+++ src/pages/coordinator.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import CeremonyVenuePDF from '../assets/venueplanceremony.pdf';
 
 const Coordinator = () => {
     const pageLinks = [
-        { title: 'Reception Schedule', url: '/' },
-        { title: 'Day Schedule', url: '/day-schedule/' },
-        { title: 'Ceremony Outline', url: '/ceremony-outline/' },
-        { title: 'Ceremony Venue Layout', url: '/static/docs/venueplanceremony.pdf' },
-        { title: 'Inventory List', url: '/inventory/' },
-        { title: 'Setup To Do List', url: '/setup/' },
-        { title: 'Tear Down To Do List', url: '/tear-down/' }
+        { title: 'Reception Schedule', url: '/', asset: false },
+        { title: 'Day Schedule', url: '/day-schedule/', asset: false },
+        { title: 'Ceremony Outline', url: '/ceremony-outline/', asset: false },
+        { title: 'Ceremony Venue Layout', url: CeremonyVenuePDF, asset: true },
+        { title: 'Inventory List', url: '/inventory/', asset: false },
+        { title: 'Setup To Do List', url: '/setup/', asset: false },
+        { title: 'Tear Down To Do List', url: '/tear-down/', asset: false }
     ]
 
     return (
@@ -17,18 +18,20 @@ const Coordinator = () => {
             <h1>Welcome to the Coordinator Page</h1>
             <p>Note from Darian and Don:</p>
             <p>
-                Thank you so much for being a part of our wedding. 
+                Thank you so much for being a part of our wedding.
                 We truly appreciate all of your help in putting this together!
-                All of this wonderful celebration couldn't happen without you! 
+                All of this wonderful celebration couldn't happen without you!
             </p>
 
             <h2>Links to various information for the wedding</h2>
             <ul>
                 {
-                    pageLinks.map(
-                        (val, idx) => 
-                        <li key={idx}><Link to={val.url}>{val.title}</Link></li>
-                    )
+                    pageLinks.map((val, idx) => {
+                        const resolvedLink = val.asset
+                            ? <a href={val.url}>{val.title}</a>
+                            : <Link to={val.url}>{val.title}</Link>;
+                        return <li key={idx}>{resolvedLink}</li>;
+                    })
                 }
             </ul>
         </div>
